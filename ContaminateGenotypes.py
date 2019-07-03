1cce8510d0d030fecc932a78381d4e949101762b
TCLamnidis <thisseass@gmail.com>
2019-07-03 16:05:17 +0200

    Added overlapOnly option. Indexing and Input check fuctions now in Utils.py.

--- ContaminateGenotypes.py
+++ ContaminateGenotypes.py
@@ -1,60 +1,30 @@
 #!/projects1/tools/anaconda3/4.0.0/bin/python3
 
 import sys, argparse, sh, random
+import Utils as util
 
-def Indexing (SampleList, Contaminant):
-    Index={}
-    Sex={}
-    Pop={}
-    Targets="{}\|"*(len(SampleList))
-    Targets+="{}"
-    for line in sh.grep(sh.cat("-n",args.Input+".ind"), Targets.format(*SampleList,Contaminant),_ok_code=[0,1]):
-        fields=line.strip().split()
-        if fields[1] in SampleList or fields[1] == Contaminant:
-            Index[fields[1]]=(int(fields[0]) -1)
-            Sex [fields[1]]=fields[2]
-            Pop [fields[1]]=fields[3]
-    return (Index, Sex, Pop)
-
-def Contaminate(Genos, SampleList, Contaminant, rates, nrReps, Index):
+def Contaminate(Genos, SampleList, Contaminant, rates, nrReps, Index, overlapOnly):
     Contaminated=""
     for Sample in SampleList:
         if Genos[Index[Sample]]!="9":
-            for rate in rates:
-                for Rep in range (nrReps):
-                    if random.uniform(0,1) <= rate:
-                        if Genos[Index[Contaminant]]=="1":
-                            if random.uniform(0,1) <= 0.5:
-                                Contaminated+="2"
+            if overlapOnly and Genos[Index[Contaminant]] == "9":
+                Contaminated+="9"*nrReps*len(rates)
+            else:
+                for rate in rates:
+                    for Rep in range (nrReps):
+                        if random.uniform(0,1) <= rate:
+                            if Genos[Index[Contaminant]]=="1":
+                                if random.uniform(0,1) <= 0.5:
+                                    Contaminated+="2"
+                                else:
+                                    Contaminated+="0"
                             else:
-                                Contaminated+="0"
+                                Contaminated+=Genos[Index[Contaminant]]
                         else:
-                            Contaminated+=Genos[Index[Contaminant]]
-                    else:
-                        Contaminated+=Genos[Index[Sample]]
+                            Contaminated+=Genos[Index[Sample]]
         else:
             Contaminated+="9"*nrReps*len(rates)
-    return (Genos+Contaminated)
-
-def CheckInputFiles(Input):
-    ##Check geno and snp compatibility
-    lineNo = ""
-    for line in sh.grep(sh.wc("-l", Input+".geno", Input+".snp"), Input):
-        if lineNo=="":
-            lineNo=line.strip().split()[0]
-        elif lineNo==line.strip().split()[0]:
-            break
-        elif lineNo!=line.strip().split()[0]:
-            raise IOError("Input .snp and .geno files do not match.")
-
-    ##Check geno and ind compatibility
-    with open(Input+".geno", "r") as f:
-        for line in f:
-            if str(len(line.strip())) == sh.wc("-l", Input+".ind").strip().split()[0]:
-                break
-            else:
-                raise IOError("Input .ind and .geno files do not match.")
-
+    return (Genos+Contaminated);
 
 ##MAIN##
 parser = argparse.ArgumentParser(usage="%(prog)s (-i <INPUT FILE PREFIX>) (-o <OUTPUT FILE PREFIX>) (-s <SAMPLE>) (-c <CONTAMINANT>) (-r <RATE1,RATE2,RATE3,...>) (-n <nrReps>)" , description="A tool artificially contaminate the genotypes of multiple sample individuals with genotypes from a contaminant individual, at different rates of contamination.")
@@ -65,6 +35,7 @@ parser.add_argument("-s", "--Samples", type=str, metavar="<SAMPLE1,SAMPLE2,SAMPL
 parser.add_argument("-c", "--Contaminant", type=str, metavar="<CONTAMINANT>", required=True, help="The contaminant individual, which will be used to contaminate the genotypes of each <SAMPLE> at the specified rate(s).")
 parser.add_argument("-r", "--rates", type=str, metavar="<RATE1,RATE2,RATE3,...>", required=True, help="A comma separated list of contamination rates.")
 parser.add_argument("-n", "--nrReps", type=int, metavar="<nrReps>", required=False, default=1, help="An integer value specifying the number of replicate contaminated genotypes to be created per contamination rate [1].")
+parser.add_argument("-v", "--overlapOnly", action="store_true", required=False, help="When provided, only SNPs that are present in both Contaminant and Source will be contaminated, while all other SNPs will be set to missing. This ensures conaminant genotypes will not be diluted due to lack of coverage overlap in the Source and Contaminant.")
 args = parser.parse_args()
 
 GenoFile = open(args.Input+".geno", "r")
@@ -79,15 +50,27 @@ SampleList=[x for x in args.Samples.split(',')]
 rates=[float(r) for r in args.rates.split(',')]
 
 ##Check for errors in input files
-CheckInputFiles(args.Input)
+util.CheckInputFiles(args.Input)
 
 ## Index Individual in database.
-(Index,Sex,Pop)=Indexing(SampleList, args.Contaminant)
+(Index,Sex,Pop)=util.Indexing(args.Input, SampleList, args.Contaminant)
+
+## Print Logfile.
+with sys.stderr as o:
+    print('#Input files:', args.Input+".geno", args.Input+".snp", args.Input+".ind", sep="\n\t", file=o)
+    print('#Output files:', args.Output+".geno", OutSnpFile, OutIndFile, sep="\n\t", file=o)
+    print('#Samples:', end="\t", file=o)
+    print(*SampleList, sep=",", file=o)
+    print('#Contamination Rates:', end="\t", file=o)
+    print(*rates, sep=",", file=o)
+    print('#NrReps:',args.nrReps, sep="\t",file=o)
+    if args.overlapOnly:
+        print('#Overlap Only option detected. All other sites will be set to missing.', file=o)
 
 ##Contaminate Genotypes
 for line in GenoFile:
     Genos=line.strip()
-    print (Contaminate(Genos, SampleList, args.Contaminant, rates, args.nrReps, Index), file=OutGenoFile)
+    print (Contaminate(Genos, SampleList, args.Contaminant, rates, args.nrReps, Index, args.overlapOnly), file=OutGenoFile)
 
 ##Copy original .snp and .ind files
 sh.cp(args.Input+".snp", OutSnpFile)
